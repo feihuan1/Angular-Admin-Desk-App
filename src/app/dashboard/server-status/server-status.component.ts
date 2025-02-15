30f6bbaab60cb0d844c1c77515760c3964b28e7e
Feihuan Peng <ericrealtor1997@gmail.com>
2025-02-14 22:41:07 -0800

    template variable

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,17 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, Input, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
   standalone: true,
   imports: [],
   templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+  styleUrl: './server-status.component.css',
 })
 
 // implements is not required but recommended, prevent pain in the ass typo
 export class ServerStatusComponent implements OnInit {
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
-
+  // private interval?: ReturnType<typeof setInterval>
+  private destroyRef = inject(DestroyRef)
   // constructor runs whenever this class is instantiated
   // constructor() {
   //   setInterval(() => {
@@ -27,21 +28,31 @@ export class ServerStatusComponent implements OnInit {
   //   }, 5000)
   // }
 
-// both init and constructor works but it is better practise to  use this !!!
+  // both init and constructor works but it is better practise to  use this !!!
   // onInit run after initialized all input witch constructor might cause error!!!
   // life cycle hooks
+  // constructor -> ngOnchanges -> ngOnInit -> ngDoCheck -> 2 branch -> afterRender
+  
   ngOnInit() {
-    setInterval(() => {
-      const rnd = Math.random()
+    const interval = setInterval(() => {
+      const rnd = Math.random();
 
-      if(rnd < 0.5) {
-        this.currentStatus = 'online'
-      } else if(rnd < 0.9) {
-        this.currentStatus = 'offline'
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
       } else {
-        this.currentStatus = 'unknown'
+        this.currentStatus = 'unknown';
       }
-    }, 5000)
+    }, 5000);
+
+    this.destroyRef.onDestroy(()=> {
+      clearInterval(interval)
+    })
   }
 
+  // // destroy interval when component not active
+  // ngOnDestroy(): void {
+  //   clearTimeout(this.interval)
+  // }
 }
